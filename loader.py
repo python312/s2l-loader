89179839677b3a77b7a2d0613a4f1f1e8e7ea8e8
python312 <smtpexfiltration@outlook.com>
2024-08-12 21:07:02 +0500

    got rid of unneccesary imports
    importing sys makes the executable file too messy

--- loader.py
+++ loader.py
@@ -2,16 +2,10 @@ import argparse
 import envInit
 #import downloadNeccesaryLibs
 import runGUI
-import sys
+
 
 def main():
-    # Set up argument parser
-    parser = argparse.ArgumentParser(description="Setup and run script in a Conda environment.")
-    parser.add_argument("--use-cuda", action="store_true", help="Specify to install CUDA version of PyTorch.")
-   # parser.add_argument("--zip", required=True, help="URL to necessary files to exec.")
-    parser.add_argument("--script", required=False,default='main.py' ,help="File name for executing. Defaults to main.py.")
-    args = parser.parse_args()
-
+    
     env_name = "cellpose_env"
     python_version = "3.8"
     
@@ -28,22 +22,19 @@ def main():
         envInit.create_conda_env(env_name, python_version)
         envInit.install_cellpose(env_name)
         
-        if args.use_cuda:
-            envInit.remove_torch(env_name)
-            envInit.install_latest_torch(env_name, use_cuda=True)
-        else:
-            envInit.install_latest_torch(env_name, use_cuda=False)
+        
+        envInit.install_latest_torch(env_name, use_cuda=False) #Got a little messy and is a pain to setup the other libraries again. use_cuda stays
         
         envInit.install_additional_libraries(env_name)
     else:
         print(f"Conda environment '{env_name}' already exists. Skipping environment setup.")
 
     # Download necessary libraries
-    #downloadNeccesaryLibs.main(args.zip)
+    
 
     # Run the main script inside the Conda environment
     
-    fullPath = f"AutomationFolder/{args.script}"
+    fullPath = f"AutomationFolder/main.py"
     
     
     runGUI.create_and_run_batch_file(env_name, fullPath, condaPath)
